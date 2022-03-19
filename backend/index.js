19923775090b7177d4d576d78a3b15cac46c7708
Sandip <sandip.pokar2724@gmail.com>
2022-03-19 18:59:07 +0530

    model create

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,15 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
+//available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Sandip!')
 })
 
 app.listen(port, () => {
